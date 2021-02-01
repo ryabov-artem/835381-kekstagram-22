821f24b49f835e05588c0580f4f18b26e737ef98
Artem <art.rya@gmail.com>
2021-02-01 14:30:50 +0500

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const random = function (min, max) {
+  // В случае, если минимальное число больше максимального, появится ошибка.
+  if (min > max) {
+    return 'Что-то пошло не так :('
+  }
+  // Честно украдено с источника (https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+random(1, 20)
+
+const length = function (str, maxLength) {
+  return str.length <= maxLength;
+}
